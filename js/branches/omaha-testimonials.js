6dffb989ed67132786f7efbd22dc374c1d4f6889
Jason Haeger <jhaeger@users.noreply.github.com>
2018-02-20 17:02:41 -0600

    Updated to be easier to reason about

--- js/branches/omaha-testimonials.js
+++ js/branches/omaha-testimonials.js
@@ -1,5 +1,5 @@
 //Utility Functions
-var getTotals = function () {
+function getTotals() {
     var num = 0;
     var photos = 0;
     var totals = ("there are " + num + " testimonials" + " and " + photos + " photographs.");
@@ -10,25 +10,36 @@ var getTotals = function () {
         photos = photos + 1;
     });
     console.log(("there are " + num + " testimonials" + " and " + photos + " photographs."));
-};
+}
+
+function organizeTestimonials() {
+    jQuery('#testimonials > p:even').each(function () {
+        jQuery(this).next().addBack().wrapAll('<li class="testimonial"></li>');
+    });
+    jQuery('p.quote').wrap("<blockquote class='ttfmake-testimonial'></blockquote>");
+    jQuery("li.testimonial").wrapInner("<center></center>");
+}
+
+function sortTestimonialsAndClients() {
+    jQuery('ul#testimonials p:even').addClass('quote');
+    jQuery('ul#testimonials p:odd').addClass('client');
+}
+
+//Run The Testimonials Getter!
 jQuery(document).ready(function (jQuery) {
     window.script_cycle = function () {
         jQuery('#testimonials').cycle();
-    }
-    jQuery("ul#testimonials").load('https://rachelpierce.benchmark.us/testimonials/ #builder-section-1456787677-column-1 blockquote p,#builder-section-1456787677-column-1 p', function () {
-        jQuery('p:even').addClass('quote');
-        jQuery('p:odd').addClass('client');
-    });
-    jQuery("ul#testimonials").load('https://ericalmquist.benchmark.us/testimonials/ #builder-section-1456787677-column-1 blockquote p,#builder-section-1456787677-column-1 p', function () {
-        jQuery('p:even').addClass('quote');
-        jQuery('p:odd').addClass('client');
-        jQuery('#testimonials > p:even').each(function () {
-            jQuery(this).next().addBack().wrapAll('<li class="testimonial"></li>');
-        });
-        jQuery('p.quote').wrap("<blockquote class='ttfmake-testimonial'></blockquote>");
-        jQuery("li.testimonial").wrapInner("<center></center>");
-        window.script_cycle();
-        getTotals();
-    });
+    };
     
+    jQuery(window).on('load', function () {
+        jQuery("ul#testimonials").load('https://rachelpierce.benchmark.us/testimonials/ #builder-section-1456787677-column-1 blockquote p,#builder-section-1456787677-column-1 p', function () {
+            sortTestimonialsAndClients();
+        });
+        jQuery("ul#testimonials").load('https://ericalmquist.benchmark.us/testimonials/ #builder-section-1456787677-column-1 blockquote p,#builder-section-1456787677-column-1 p', function () {
+            sortTestimonialsAndClients();
+            organizeTestimonials();
+            window.script_cycle();
+            getTotals();
+        });
+    });
 });
